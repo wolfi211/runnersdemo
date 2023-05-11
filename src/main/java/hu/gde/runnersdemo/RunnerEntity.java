ad00875586593f00b1d953825eb7d8421873b418
Daniel Wolf <wolf.dani9@gmail.com>
2023-05-11 15:17:26 +0200

    created age field for Runners

--- src/main/java/hu/gde/runnersdemo/RunnerEntity.java
+++ src/main/java/hu/gde/runnersdemo/RunnerEntity.java
@@ -11,6 +11,7 @@ public class RunnerEntity {
     private long runnerId;
     private String runnerName;
     private long averagePace;
+    private long age;
 
     @OneToMany(mappedBy = "runner", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LapTimeEntity> laptimes = new ArrayList<>();
@@ -31,6 +32,10 @@ public class RunnerEntity {
         return averagePace;
     }
 
+    public long getAge() {
+        return age;
+    }
+
     public void setRunnerId(long runnerId) {
         this.runnerId = runnerId;
     }
@@ -46,4 +51,9 @@ public class RunnerEntity {
     public List<LapTimeEntity> getLaptimes() {
         return laptimes;
     }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+
 }
